d65756a2ff747a16184c43ca7f33f3500b64699b
Nitin Kaushal <nkcodedev.chd@gmail.com>
2026-07-01 13:35:00 +0530

    docs: add GitHub profile README

---
+++ README.d
@@ -0,0 +1,52 @@
+# Hi 👋 I'm Nitin Kaushal
+
+🏗 Senior Solution Architect
+
+🚀 Creator of Resili
+
+☁ AWS Certified
+
+💻 TypeScript • Node.js • Cloud • Distributed Systems
+
+---
+
+## 🚀 Open Source
+
+### 🛡 Resili
+
+Modern TypeScript Resilience Toolkit
+
+- Retry
+- Timeout
+- Circuit Breaker
+- Bulkhead
+- Rate Limiter
+
+👉 https://github.com/nkcodedev/resili
+
+---
+
+## 🌐 Connect
+
+
+- GitHub: https://github.com/nkcodedev
+- npm: https://www.npmjs.com/~nkcodedev
+- Medium: https://medium.com/@nitink4107
+
+---
+
+## 🛠 Tech Stack
+
+TypeScript • Node.js • AWS • Terraform • Docker • PostgreSQL • DynamoDB • Serverless • System Design
+
+---
+
+## 📚 Currently Building
+
+- 🛡 Resili
+- 🌐 Developer Tools
+- 📘 System Design Resources
+
+---
+
+⭐ Thanks for visiting my profile!
